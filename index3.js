f6d2b499447ca47927c23947c7ef7c9b112d5160
Romain L <51294979+TisoOfficiel@users.noreply.github.com>
2022-06-13 23:56:28 +0200

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,38 +1,16 @@
-const Discord = require("discord.js");
 const dotenv = require('dotenv');
 const path = require('node:path');
-const fs= require("fs");
+
 dotenv.config();
-const client = new Discord.Client({
-    intents:[
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.DIRECT_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-        
-    ],
-    partials: ['MESSAGE','CHANNEL','REACTION']
 
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
 });
-
-    
-
-const prefix = "!";
-client.commands = new Discord.Collection();
-
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-   
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
 client.login(process.env.DISCORD_TOKEN);
